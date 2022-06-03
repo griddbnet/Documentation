4b9dfd70b23986c8edd7dea7ac66f0775d3bf667
imru@fixstars.com <imru@fixstars.com>
2022-06-02 19:35:57 -0700

    adding building versioning

---
+++ build-docs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+git fetch --all
+
+home=$(pwd)
+#branch=$(git rev-parse --abbrev-ref HEAD)
+branch="latest"
+new="v5.0"
+
+mkdir $branch
+mkdir $new
+
+cd $home/docs
+npm install
+
+cp -r $home/docs/* $home/$branch
+
+git checkout $new
+cp -r $home/docs/* $home/$new
+
+#mv .vuepress/dist/* .vuepress/$new
+#mv .vuepress/$new .vuepress/dist
+#mv $home/$branch .vuepress/dist
+#cp .vuepress/dist/$new/* .vuepress/dist
+
+#rm -rf $home/$new
+
+npm run build
+mv $home/$branch $home/docs/.vuepress/dist
+mv $home/$new $home/docs/.vuepress/dist
+git checkout $branch
+
--- docs/ja/about/griddb-license.md
+++
@@ -1,1 +0,0 @@
-# GridDB license
