68249600a6d3f0ab25de1920b3fae0b5f73e8324
imru@fixstars.com <imru@fixstars.com>
2022-08-24 10:53:06 -0700

    fixed run dev and fixed japanese broken link

--- run-dev.sh
+++ run-dev.sh
@@ -3,8 +3,15 @@
 home=$(pwd)
 
 mkdir $home/latest
+mkdir $home/latest-ja
+
 cp -r $home/docs/* $home/latest
+cp -r $home/docs/ja/* $home/latest-ja
+
 mv $home/latest $home/docs
+mv $home/latest-ja $home/docs/ja
+mv $home/docs/ja/latest-ja $home/docs/ja/latest
+
 cd $home/docs/
 npm install
 npm run dev
