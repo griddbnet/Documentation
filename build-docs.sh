0046f5079a24c4c882db5c387afa8af51e943c9a
israel imru <imru@fixstars.com>
2022-08-25 14:47:14 -0700

    fixing up script

--- build-docs.sh
+++ build-docs.sh
@@ -7,45 +7,37 @@ thisBranch="latest"
 echo "thisBranch = $thisBranch"
 oldBranch="v4.5"
 
-
-echo "Making directories $thisBranch && $oldBranch"
-mkdir $oldBranch
-mkdir $oldBranch-ja
-
 cd $home/docs
 echo "Copying contents of LATEST docs to home root"
-cp -r $home/docs/latest $home/
-cp -r $home/docs/ja $home/
+rsync -avzh $home/docs/latest/ $home/latest
+rsync -avzh $home/docs/ja/ $home/ja
+
+#cp -r $home/docs/latest $home/
+#cp -r $home/docs/ja $home/
 
 
 echo "changing branch to $oldBranch"
 git checkout $oldBranch
 echo "copying over older docs to directory called $oldBranch"
-cp -r $home/docs/* $home/$oldBranch
-cp -r $home/docs/ja/* $home/$oldBranch-ja
+rsync -avzh $home/docs/ $home/$oldBranch --exclude $home/docs/ja
+rsync -avzh $home/docs/ja/ $home/ja/v4.5
+
+#cp -r $home/docs/* $home/$oldBranch
+#cp -r $home/docs/ja/* $home/$oldBranch-ja
 
 echo "running install"
 npm install
 echo "copying contents to docs for building"
 
 mv $home/latest $home/docs/
-echo "renaming ja content to $thisBranch and moving to docs/ja"
-mv $home/ja $home/latest
-mv $home/latest $home/ja
-echo "moving $oldBranch from root to docs/"
+mv $home/ja $home/docs/
 mv $home/$oldBranch $home/docs/
-
-echo "renaming $oldBranch-ja within docs/ja directory"
-mv $home/$oldBranch-ja $home/docs/ja
-mv $home/docs/ja/$oldBranch-ja $home/docs/ja/$oldBranch
 
 echo "going to run build"
 npm run build
 
 echo "deleting directories: docs/$thisBranch and docs/$oldBranch and docs/node_modules"
-rm -rf $home/docs/$thisBranch
 rm -rf $home/docs/$oldBranch
-rm -rf $home/docs/ja/$thisBranch
 rm -rf $home/docs/ja/$oldBranch
 rm -rf $home/docs/node_modules
 rm $home/docs/package-lock.json
