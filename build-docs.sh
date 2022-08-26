1e98d83d6d67b6f3f872ac721e0ebbc721a0a2b8
israel imru <imru@fixstars.com>
2022-08-25 17:11:19 -0700

    Fix

--- build-docs.sh
+++ build-docs.sh
@@ -19,7 +19,7 @@ echo "changing branch to $oldBranch"
 git checkout $oldBranch
 # echo "copying over older docs to directory called $oldBranch"
 # rsync -av $home/docs/$oldBranch/ $home/$oldBranch --exclude $home/docs/ja
-mv $home/docs/$oldBranch/ja/$oldBranch $home/ja/
+mv $home/docs/ja/$oldBranch $home/ja/
 
 
 #cp -r $home/docs/* $home/$oldBranch
