d780cb5788ca0ecdca585d16163645aa71223e40
imru@fixstars.com <imru@fixstars.com>
2022-11-16 09:30:35 -0800

    fixed run-dev script

--- run-dev.sh
+++ run-dev.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-home=$(pwd)
-
-mkdir $home/latest
-mkdir $home/latest-ja
-
-cp -r $home/docs/* $home/latest
-cp -r $home/docs/ja/* $home/latest-ja
-
-mv $home/latest $home/docs
-mv $home/latest-ja $home/docs/ja
-mv $home/docs/ja/latest-ja $home/docs/ja/latest
-
-cd $home/docs/
+cd docs/
 npm install
 npm run dev
