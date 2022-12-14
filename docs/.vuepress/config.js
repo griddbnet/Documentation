48008d6937dcaf553e725dec5371fe9496ccd033
imru@fixstars.com <imru@fixstars.com>
2022-12-14 15:20:52 -0800

    Added in the edit this page and last updated to each page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -91,6 +91,22 @@ module.exports = {
   },
   themeConfig: {
     logo: '/logo.png',
+    repo: 'griddbnet/Documentation',
+    // Customising the header label
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    repoLabel: 'Contribute!',
+
+    // Optional options for generating "Edit this page" link
+
+    // if your docs are not at the root of the repo:
+    docsDir: 'docs',
+    // if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: 'main',
+    // defaults to false, set to true to enable
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: 'Help us improve this page!',
+    lastUpdated: 'Last Updated', // string | boolean
     locales: {
       '/': {
         selectText: 'Languages',
