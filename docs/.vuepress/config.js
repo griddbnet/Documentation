bb815fa2f82b6110c8c925fe50702eed5feb5344
imru@fixstars.com <imru@fixstars.com>
2022-12-21 17:22:50 -0800

    removed the github type links

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -91,19 +91,6 @@ module.exports = {
   },
   themeConfig: {
     logo: '/logo.png',
-    repo: 'griddbnet/Documentation',
-
-    // Optional options for generating "Edit this page" link
-
-    // if your docs are not at the root of the repo:
-    docsDir: 'docs',
-    // if your docs are in a specific branch (defaults to 'master'):
-    docsBranch: 'main',
-    // defaults to false, set to true to enable
-    editLinks: true,
-    // custom text for edit link. Defaults to "Edit this page"
-    editLinkText: 'Help us improve this page!',
-    lastUpdated: 'Last Updated', // string | boolean
     locales: {
       '/': {
         selectText: 'Languages',
